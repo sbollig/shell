e2224eb609e42f3ecf3fc9fc9de069039fc6d33c
Scott A Bollig <sbollig@gmail.com>
2013-09-13 12:22:48 -0700

    selectperlbrew.pl add regex to only print

--- bin/selectperlbrew.pl
+++ bin/selectperlbrew.pl
@@ -5,7 +5,7 @@ use v5.10;
 
 say "Please select the version of perlbrew from the list: ";
 my @pb_list = `perlbrew list`;
-print "@pb_list\n"; 
+#rint "@pb_list\n"; 
 for my $version ( values @pb_list ) {
-    print $version;
+    print $version if $version =~ /perl-/;
 }
