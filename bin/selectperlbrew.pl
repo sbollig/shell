9cbc5244eb9e21e5021771a9bbd37210ce99fcc3
Scott A Bollig <sbollig@gmail.com>
2013-09-13 12:50:44 -0700

    selectperlbrew.pl adding print foreach test

--- bin/selectperlbrew.pl
+++ bin/selectperlbrew.pl
@@ -6,9 +6,12 @@ use v5.10;
 say "Please select the version of perlbrew from the list: ";
 my @pb_list = `perlbrew list`;
 my $num = 0;
+my %versions;
 for my $version ( values @pb_list ) {
     chomp $version;
     $num++;
     $version = $1 if $version =~ /perl-(\d\.\d\d\.\d).?/;
+    $versions{$num}=$version;
     print "$num\. $version\n";
 }
+for my $version ( values %versions ) { say $version };
