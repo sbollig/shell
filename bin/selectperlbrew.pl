1649996616d57f2daffa75e2da726f754132aa2d
Scott A Bollig <sbollig@gmail.com>
2013-09-13 12:42:23 -0700

    selectperlbrew.pl add regex to pick up numberical version

--- bin/selectperlbrew.pl
+++ bin/selectperlbrew.pl
@@ -2,13 +2,13 @@
 use strict; 
 use warnings; 
 use v5.10;
-
+#se Regexp::Debugger;
 say "Please select the version of perlbrew from the list: ";
 my @pb_list = `perlbrew list`;
 my $num = 0;
 for my $version ( values @pb_list ) {
     chomp $version;
     $num++;
-    $version = $1 if $version =~ /^\s+(perl-.+)/;
-    print "$num\. $version\n" if $version =~ /perl-/;
+    $version = $1 if $version =~ /perl-(\d\.\d\d\.\d).?/;
+    print "$num\. $version\n";
 }
