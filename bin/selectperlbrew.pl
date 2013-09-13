830c23eefe9ab4cfe2cb0fe0cfa808319c16b43a
Scott A Bollig <sbollig@gmail.com>
2013-09-13 12:32:27 -0700

    selectperlbrew.pl clean up spaces

--- bin/selectperlbrew.pl
+++ bin/selectperlbrew.pl
@@ -5,9 +5,10 @@ use v5.10;
 
 say "Please select the version of perlbrew from the list: ";
 my @pb_list = `perlbrew list`;
-#rint "@pb_list\n"; 
 my $num = 0;
 for my $version ( values @pb_list ) {
+    chomp $version;
     $num++;
-    print "$num\. $version" if $version =~ /perl-/;
+    $version = $1 if $version =~ /^\s+(perl-.+)/;
+    print "$num\. $version\n" if $version =~ /perl-/;
 }
